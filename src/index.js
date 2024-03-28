fe4cc21c3c5b8431e7d83d8e38bd0b2c0719f5c9
Bekir Faruk ARABACI <113997401+BekirFarukARABACI@users.noreply.github.com>
2024-03-29 00:38:28 +0300

    Router öğrenildi

--- src/index.js
+++ src/index.js
@@ -372,7 +372,7 @@ root.render(
     <Garage/>
   </>
 )
-*/
+
 
 
 import React from "react";
@@ -448,5 +448,39 @@ root.render(
   <>
     <h1>Forms</h1>
     <MyForm />
+  </>
+)
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Blogs from "./pages/blogs";
+import Contact from "./pages/contact";
+import NoPage from "./pages/nopage";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+export default function App(){
+  return(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index  element={<Home/>}/>
+            <Route path="blogs"  element={<Blogs/>}/>
+            <Route path = "contact"  element={<Contact/>}/>
+            <Route path = "*"  element={<NoPage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+root.render(
+  <>
+    <h1>Router</h1>
+    <hr/>
+    <App/>
   </>
 )
