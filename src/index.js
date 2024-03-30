c2f3ff266045a6d72c18e880b86e11bfd8c37b8b
Bekir Faruk ARABACI <113997401+BekirFarukARABACI@users.noreply.github.com>
2024-03-30 19:53:33 +0300

    useMemo öğrenildi

--- src/index.js
+++ src/index.js
@@ -788,6 +788,7 @@ export default function App() {
 root.render(<App />)
 
 */
+/*
 
 import React , {useCallback, useState}  from "react";
 import ReactDOM from "react-dom/client";
@@ -818,6 +819,56 @@ export default function App() {
       </div>
     </>
   )
+}
+
+root.render(<App />)
+
+*/
+
+import React, { useMemo, useState } from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+export default function App() {
+  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  const calculation = useMemo(()=>expensiveCalculation(count),[count])
+
+  const increment = () => {
+    setCount((c) => c + 1)
+  }
+
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"])
+  }
+  return (
+    <>
+      <h1>useMemo</h1>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return (<p key={index} > {todo}</p>)
+        })}
+        <button onClick={addTodo}> Add Todo</button>
+      </div>
+      <hr/>
+      <div>
+        Count : {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+      </div>
+    </>
+  )
+}
+
+const expensiveCalculation = (num) => {
+  console.log("Calculating")
+  for (let i = 0; i < 100000000; i++) {
+    num += 1
+  }
 }
 
 root.render(<App />)
